d12d1637e8ad66a417aaeb7f61ce555786fd160d
Tommy <tmydali0@gmail.com>
2020-12-19 02:13:19 +0800

    FSM prototype

--- app.py
+++ app.py
@@ -7,34 +7,11 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-from fsm import TocMachine
+from fsm import FSMInitialize
 from utils import send_text_message
-
 load_dotenv()
 
-
-machine = TocMachine(
-    states=["user", "state1", "state2"],
-    transitions=[
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
-        },
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
-        },
-        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
-    ],
-    initial="user",
-    auto_transitions=False,
-    show_conditions=True,
-)
-
+machine = FSMInitialize()
 app = Flask(__name__, static_url_path="")
 
 
--- fsm.py
+++ fsm.py
@@ -1,36 +1,223 @@
 from transitions.extensions import GraphMachine
+import json
 
 from utils import send_text_message
 
+def getAPODlink(date_offset=0):
+    return f"<url> {date_offset}"
 
-class TocMachine(GraphMachine):
+def getYTlink(word1):
+    return word1 + " <url>"
+
+class AstroMindMachine(GraphMachine):
+
+    date_offset = 0
+    scenario = {}
+
     def __init__(self, **machine_configs):
-        self.machine = GraphMachine(model=self, **machine_configs)
+        super().__init__(model=self, **machine_configs)
+        with open("scenario.json", encoding="utf-8") as f:
+            self.scenario = json.load(f)
 
-    def is_going_to_state1(self, event):
+    # Behavior on transitions
+    def on_enter_Q1(self, event):
+        q = self.scenario["Q1"]["content"]
+        print(q)
+
+    def on_enter_Q2_1(self, event):
+        q = self.scenario["Q2"]["content"]
+        print(q)
+
+    def on_enter_Q2_2(self, event):
+        q = self.scenario["Q2"]["content"]
+        print(q)
+
+    def on_enter_Q2_3(self, event):
+        q = self.scenario["Q2"]["content"]
+        print(q)
+
+    def on_exit_Q2_1(self, event):
+        word1 = self.scenario["Q1"]["options"][0]
+        data = event.postback.data
+        tokens = data.split("&!")
+        word2 = tokens[-1]
+        reply = self.scenario["Q2"]["template"].format(word1, word2)
+        print(reply)
+
+    def on_exit_Q2_2(self, event):
+        word1 = self.scenario["Q1"]["options"][1]
+        data = event.postback.data
+        tokens = data.split("&!")
+        word2 = tokens[-1]
+        reply = self.scenario["Q2"]["template"].format(word1, word2)
+        print(reply)
+
+    def on_exit_Q2_3(self, event):
+        word1 = self.scenario["Q1"]["options"][2]
+        data = event.postback.data
+        tokens = data.split("&!")
+        word2 = tokens[-1]
+        reply = self.scenario["Q2"]["template"].format(word1, word2)
+        print(reply)
+
+    def on_enter_APOD(self, event):
+        data = event.postback.data
+        tokens = data.split("&!")
+        option = tokens[-1]
+        if option == self.scenario["APOD"]["options"][0]:
+            print("How about this?")
+        img_link = getAPODlink(self.date_offset)
+        print(img_link)
+
+    def on_enter_Q3(self, event):
+        q = self.scenario["Q3"]["content"]
+        print(q)
+
+    def on_exit_Q3(self, event):
+        data = event.postback.data
+        tokens = data.split("&!")
+        word1 = tokens[-1]
+        reply = self.scenario["Q3"]["template"].format(word1)
+        link = getYTlink(word1)
+        print(reply)
+        print(link)
+
+    def on_enter_End(self, event):
+        q = self.scenario["End"]["content"]
+        print(q)
+
+    # Conditions
+    def isOption1(self, event):
+        data = event.postback.data
+        tokens = data.split("&!")
+        options = self.scenario["Q1"]["options"]
+        return tokens[-1] == options[0]
+
+    def isOption2(self, event):
+        data = event.postback.data
+        tokens = data.split("&!")
+        options = self.scenario["Q1"]["options"]
+        return tokens[-1] == options[1]
+
+    def isOption3(self, event):
+        data = event.postback.data
+        tokens = data.split("&!")
+        options = self.scenario["Q1"]["options"]
+        return tokens[-1] == options[2]
+
+    def isQ2Option(self, event):
+        data = event.postback.data
+        tokens = data.split("&!")
+        options = self.scenario["Q2"]["options"]
+        return tokens[-1] in options
+
+    def isQ3Option(self, event):
+        data = event.postback.data
+        tokens = data.split("&!")
+        options = self.scenario["Q3"]["options"]
+        return tokens[-1] in options
+
+    def isToStart(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
+        options = self.scenario["End"]["options"]
+        return text in options
 
-    def is_going_to_state2(self, event):
+    def isAPODOption(self, event):
         text = event.message.text
-        return text.lower() == "go to state2"
+        options = self.scenario["APOD"]["options"]
+        return text in options
 
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+def FSMInitialize():
+    machine = AstroMindMachine(
+        states=["User", "Q1", "Q2_1", "Q2_2", "Q2_3", "APOD", "Q3", "End"],
+        transitions=[
+            ### Normal flow ###
+            # Psycho quiz
+            {
+                "trigger": "advance",
+                "source": "User",
+                "dest": "Q1",
+                "conditions": "isToStart"
+            },
+            {
+                "trigger": "advance",
+                "source": "Q1",
+                "dest": "Q2_1",
+                "conditions": "isOption1"
+            },
+            {
+                "trigger": "advance",
+                "source": "Q1",
+                "dest": "Q2_2",
+                "conditions": "isOption2"
+            },
+            {
+                "trigger": "advance",
+                "source": "Q1",
+                "dest": "Q2_3",
+                "conditions": "isOption3"
+            },
 
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
-        self.go_back()
+            # Astronomical pictures
+            {
+                "trigger": "advance",
+                "source": ["Q2_1", "Q2_2", "Q2_3"],
+                "dest": "APOD",
+                "conditions": "isQ2Option"
+            },
+            {
+                "trigger": "advance",
+                "source": "APOD",
+                "dest": "APOD",
+                "conditions": "isAPODOption"
+            },
+            # YT Recommendation
+            {
+                "trigger": "advance",
+                "source": "APOD",
+                "dest": "Q3",
+            },
+            {
+                "trigger": "advance",
+                "source": "Q3",
+                "dest": "End",
+                "conditions": "isQ3Option"
+            },
+            {
+                "trigger": "advance",
+                "source": "End",
+                "dest": "Q1",
+                "conditions": "isToStart"
+            },
 
-    def on_exit_state1(self):
-        print("Leaving state1")
+            ### Escape ###
+            {
+                "trigger": "exit",
+                "source": "*",
+                "dest": "End"
+            },
+        ],
+        initial="User",
+        auto_transitions=False,
+        show_conditions=True,
+    )
+    return machine
 
-    def on_enter_state2(self, event):
-        print("I'm entering state2")
 
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
-        self.go_back()
+if __name__ == "__main__":
+    class Postback:
+        data = ''
+    class Mesg:
+        text = ''
+    class Event:
+        postback = Postback()
+        message = Mesg()
 
-    def on_exit_state2(self):
-        print("Leaving state2")
+    machine = FSMInitialize()
+    ev = Event()
+
+    while(1):
+        text = input("Wait for input: ")
+        ev.message.text = text
+        ev.postback.data = "A01&!" + text
+        machine.advance(ev)
