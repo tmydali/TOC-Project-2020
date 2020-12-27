9c6cec1d3feab5ef766e532910d4188c06159d33
Tommy <tmydali0@gmail.com>
2020-12-27 22:26:24 +0800

    Move fsm.png into /img

--- app.py
+++ app.py
@@ -101,8 +101,8 @@ def webhook_handler():
 
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
-    # machine.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
+    # machine.get_graph().draw("./img/fsm.png", prog="dot", format="png")
+    return send_file("./img/fsm.png", mimetype="image/png")
 
 
 if __name__ == "__main__":
